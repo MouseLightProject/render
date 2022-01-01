1fb09ac870d125e53a233a2aa7c17aafa6701343
Ben Arthur <arthurb@hhmi.org>
2022-01-01 16:42:06 -0500

    delete accidentally commited files

--- test/regression/renderdata.sh
+++
@@ -1,2 +0,0 @@
-ssh login1 "source /misc/lsf/conf/profile.lsf; /groups/mousebrainmicro/mousebrainmicro/Software/barycentric3/src/render/src/render ${RENDER_PATH}/src/render/test/regression/987roiprod-parameters.jl" &
-ssh login1 "source /misc/lsf/conf/profile.lsf; ${RENDER_PATH}/src/render/src/render ${RENDER_PATH}/src/render/test/regression/987roidev-parameters.jl" &
